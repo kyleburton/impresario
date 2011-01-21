83fe9fe1edb90ca8f78a516520d17863ee42a817
Kyle Burton <kburton@relaynetwork.com>
2011-01-20 23:06:33 -0500

    just focusing on transitions, will enhance after functional this time :)

--- src/impresario/core.clj
+++ src/impresario/core.clj
@@ -5,166 +5,54 @@
 ;; values are descriptions of the state, including outbound transitions
 ;; observers may be attached to the workflow
 
-(defn spec-start-states [spec]
-  (filter #(:start (get (:states spec) %))
-          (keys (:states spec))))
-
-;; TODO: fore transition predicates, make-workflow supports either
-;; keywords (qualified with namespace:
-;; impresario.core.test/close-door?) or symbols (unqualified, in which
-;; case it looks up the namespace via the meta-data) - we should
-;; ensure we can resolve all those as part of the validateion
-(defn validate! [spec]
-  (if (nil? spec)
-    (throw (IllegalArgumentException. "Error: null specification")))
-  (if (empty? (keys spec))
-    (throw (IllegalArgumentException. "Error: empty specifiction")))
-  ;; must have 1 and only 1 start state
-  (let [start-nodes (filter #(get %1 :start) (vals (:states spec)))]
-    (if-not (= 1 (count start-nodes))
-      (throw (IllegalArgumentException.
-              (format "Error: zero or multiple start states: (%s) :: must have 1 and only 1 initial state."
-                      (map :name start-nodes))))))
-  true)
-
-(defn spec-start-state [spec]
-  (first (filter (fn [state-name]
-                   (:start (get (:states spec)
-                                state-name)))
-                 (keys (:states spec)))))
-
 (defn split-keyword-parts [kwd]
   (.split (.substring (str kwd) 1)
           "/"))
 
-(defn fn-lookup-via-symbol [sym-name & args]
+
+(defn fn-lookup-via-symbol [sym-name]
   (let [[ns-name-part sym-name-part] (split-keyword-parts sym-name)]
     (ns-resolve (symbol ns-name-part)
                 (symbol sym-name-part))))
 
-(defn fn-invoke-via-symbol [sym-name & args]
-  (let [fn (fn-lookup-via-symbol sym-name)]
-    (apply fn args)))
+;; ((fn-lookup-via-symbol :impresario.test.core/transition-every-time) nil nil nil)
 
-;; (fn-invoke-via-symbol :clojure.core/printf "foo:%s %s%s" "bar" "qux" "!")
-
-(defn construct-internal-store [spec]
-  (let [internal-store (:internal-store spec)]
-    (cond
-      (keyword? internal-store)
-      (fn-invoke-via-symbol internal-store)
-
-      (map? internal-store)
-      internal-store
-
-      :else
-      (throw (IllegalArgumentException.
-              (format
-               "Error, spec's internal-store type (%s) not recognized, expected map or keyword in: %s"
-               (class internal-store)
-               spec))))))
-
-(defn symbol-to-combined-keyword [spec-ns thing]
-  ;; convert whatever thing is into a keyword that contains both the
-  ;; package and namespace so that we can serialize, reify and lookup
-  ;; and call it by that name in the future...
+(defn resolve-predicate [pred]
   (cond
-    (keyword? thing)
-    (keyword (str (.substring (str spec-ns) 1) "/" (name thing)))
-
-    (symbol? thing)
-    (let [thing-var (resolve thing)
-          thing-meta (meta thing-var)]
-      (keyword (str (str (:ns thing-meta))
-                    "/"
-                    (str (:name thing-meta)))))
+    (keyword? pred)
+    (fn-lookup-via-symbol pred)
     :else
-    (throw (IllegalArgumentException. (format "Unable to resolve: %s of %s"
-                                              thing (class thing))))))
+    (throw (RuntimeException. (format "Don't know how to resolve: '%s'" pred)))))
 
-(defn resolve-symbol-in-state [spec-ns state]
-  (merge state
-         {:transitions-to
-          (vec
-           (map (fn [transition]
-                  (printf "resolve-symbol-in-state: map body, transition=%s\n"
-                          transition)
-                  (if (:if transition)
-                    (merge transition {:if (symbol-to-combined-keyword spec-ns (:if transition))})
-                    transition))
-                (:transitions-to state)))}))
-
-;; (resolve-symbol-in-state 'some-namespace {:state :state-name :transitions-to [{:if :the-predicate-name}]})
-
-(defn resolve-symbols-in-states [spec-ns states]
-  (loop [m {}
-         [k & ks] (keys states)]
-    (cond (not k)
-          m
-          :else
-          (recur (assoc m k (resolve-symbol-in-state spec-ns (get states k)))
-                 ks))))
-
-(defn resolve-symbols-in-spec [spec]
-  (merge spec {:states (resolve-symbols-in-states
-                        (:ns spec)
-                        (:states spec))}))
+(defn can-transition-to? [workflow current-state transition-info state-store]
+  (let [pred       (resolve-predicate (:if transition-info))
+        state-name (:state transition-info)]
+    (if (pred workflow current-state state-store)
+      state-name
+      nil)))
 
 
-;; on construction, keywordize the predicates so it's easier on the users for the namespacing...
-(defn construct-workflow [spec]
-  (validate! spec)
-  {:name                (:name spec)
-   :definition          (resolve-symbols-in-spec spec)
-   :original-definition spec
-   :internal-store      (construct-internal-store spec)
-   :external-store      {}
-   :current-state       (spec-start-state spec)})
+(defn transition-once? [workflow current-state state-store]
+  (let [transitions (:transitions (get (:states workflow)
+                                       current-state))
+        viable-next-states (filter (fn [transition-info]
+                                     (can-transition-to?
+                                      workflow
+                                      current-state
+                                      transition-info
+                                      state-store))
+                                   transitions)]
+    (printf "transitions:%s\n" (vec transitions))
+    (printf "viable-next-states:%s\n" (vec viable-next-states))
+    (if (= 1 (count viable-next-states))
+      (:state (first viable-next-states))
+      nil)))
 
-(defmacro make-workflow [spec]
-  `(construct-workflow
-    (merge {:ns (keyword (str ~'*ns*))
-            :internal-store {}
-            :external-store {}}
-           ~spec)))
-
-(defn current-state [worklfow]
-  (:current-state worklfow))
-
-(defn current-state-info [workflow]
-  (get-in workflow [:definition :states (current-state workflow)]))
-
-(defn possible-transitions-from [workflow state]
-  (get-in workflow [:definition :states state :transitions-to]))
-
-
-(defn resolve-predicate [pred-ns pred-name]
-  (.println System/err
-            (format "resolve-predicate: pred-ns:%s pred-name:%s"
-                    pred-ns pred-name))
-  (printf "resolve-predicate: pred-ns:%s pred-name:%s\n"
-          pred-ns pred-name)
-  (ns-resolve (symbol (name pred-ns))
-              (symbol (name pred-name))))
-
-
-(defn can-transition? [workflow]
-  (printf "can-transition? curr-state:%s  state-info:%s\n"
-          (current-state workflow)
-          (current-state-info workflow))
-  (doseq [transition (possible-transitions-from workflow (current-state workflow))]
-    (printf "  transition:%s\n" (vec transition)))
-  (let [curr-state             (current-state workflow)
-        state-info             (current-state-info workflow)
-        transitions            (possible-transitions-from workflow curr-state)
-        viable-next-states     (filter
-                                (fn [transition]
-                                  (printf "  Filter Pred: transition=%s\n" transition)
-                                  (let [pred-name (:if transition)
-                                        pred      (resolve-predicate (:ns workflow) pred-name)]
-                                    (pred workflow)))
-                                       transitions)]
-    ;; TODO: can-transition-to? should return [ name-of-state new-workflow ]
-    viable-next-states))
-
-
+(defn transition? [workflow current-state state-store]
+  (loop [prev-state current-state
+         next-state (transition-once? workflow current-state state-store)]
+    (printf "transition? prev-state:%s next-state:%s\n" prev-state next-state)
+    (if (nil? next-state)
+      prev-state
+      (recur next-state
+             (transition-once? workflow next-state state-store)))))
--- test/impresario/test/core.clj
+++ test/impresario/test/core.clj
@@ -2,93 +2,57 @@
   (:use [impresario.core] :reload)
   (:use [clojure.test]))
 
-(deftest test-validate!
-  (is (thrown? IllegalArgumentException (validate! nil)))
-  (is (thrown? IllegalArgumentException (validate! {})))
-  (is (thrown? IllegalArgumentException (validate! {:states {:first {}}})))
-  (is (thrown? IllegalArgumentException (validate! {:states
-                                                    {:first  {:start true}
-                                                     :second {:start true}}})))
-  (is (validate! {:states {:first {:start true}}})))
+;; (def door-spec
+;;   {:name "Front Door"
+;;    ;; NB: what about constructor generate state?  Should we support a function that returns the initial state?
+;;    ;; :internal-store :some.namespace/make-initial-state
+;;    :internal-store {:locked? false
+;;                     :key-id  "761098709817145"}
+;;    ;; :triggers [{:type :timer {:timer-data-goes-here "1234 4312 abcd"}}
+;;    ;;            {:type :on-wakeup :some-function]
+;;    :states  {:open   {:start true
+;;                       :transitions [{:state :closed :if :close-door?}]}
+;;              :closed {:transitions [{:state :open   :if :can-open-door?}
+;;                                        {:state :locked :if :lock-door?}]}
+;;              :locked {:transitions [{:state :closed :if :unlock-door?}]}}})
 
-;; (validate! nil)
-;; (test-validate!)
+;; TODO: assert that would-transition? does not fire triggers/observers
+;; TODO: assert that transition-once! does fire triggers/observers
+;; TODO: assert that transition! does fire triggers/observers
 
-(deftest test-spec-start-state
-  (is (spec-start-state {:states {:first {:start true}}}))
-  (is (= :first
-         (spec-start-state {:states {:first {:start true}}}))))
+(defn transition-every-time [workflow current-state state-store]
+  (printf "transition-every-time, returning true...\n")
+  true)
+
+(def simple-workflow
+    {:states
+     {:first-state
+      {:start true
+       :transitions
+       [{:state :next-state
+         :if :impresario.test.core/transition-every-time}]}
+      :next-state
+      {:transitions
+       [{:state :final-state
+         :if :impresario.test.core/transition-every-time}]}
+      :final-state
+      {:transitions []}}})
 
 
-;; (test-spec-start-state)
+(deftest test-would-transition-once?
+    (is (= :next-state
+           (transition-once?
+            simple-workflow
+            :first-state
+            {}))))
 
-(def door-spec
-  {:name "Front Door"
-   ;; NB: what about constructor generate state?  Should we support a function that returns the initial state?
-   ;; :internal-store :some.namespace/make-initial-state
-   :internal-store {:locked? false
-                    :key-id  "761098709817145"}
-   ;; :triggers [{:type :timer {:timer-data-goes-here "1234 4312 abcd"}}
-   ;;            {:type :on-wakeup :some-function]
-   :states  {:open   {:start true
-                      :transitions-to [{:state :closed :if :close-door?}]}
-             :closed {:transitions-to [{:state :open   :if :can-open-door?}
-                                       {:state :locked :if :lock-door?}]}
-             :locked {:transitions-to [{:state :closed :if :unlock-door?}]}}})
+;; (test-would-transition-once?)
 
-;; (spec-start-states door-spec)
+(deftest test-would-transition?
+    (is (= :final-state
+           (transition?
+            simple-workflow
+            :first-state
+            {}))))
 
-;; (resolve-symbols-in-spec door-spec)
-
-(def door-workflow (make-workflow door-spec))
-
-;; (clojure.pprint/pprint door-workflow)
-
-(deftest test-start-state-door
-  (is (= :open (current-state door-workflow))))
-
-;; (possible-transitions-from door-workflow :open)
-;; (get-in door-workflow [:definition :states :open :transitions-to])
-;; door-workflow
-
-;; (test-start-state-door)
-;; (current-state-info door-workflow)
-
-(defn close-door? [workflow]
-  (printf "close-door? workflow=%s\n" workflow)
-  false)
-
-(deftest test-can-transition-door
-  (is (not (can-transition? door-workflow)))
-  (is (can-transition? (assoc-in door-workflow
-                                 [:external-store :locked] false))))
-
-;; (test-can-transition-door)
-
-;; (symbol-to-combined-keyword close-door?)
-;; (resolve close-door?)
-
-;; TODO: assert that would-transition-once? does not fire triggers/observers
-
-#_(def simple-workflow
-  (make-workflow
-   {:states
-    {:first-state
-     {:start true
-      :transitions-to
-      [{:state :next-state
-        :if (fn [& args] true)}]}
-     :next-state
-     {:transitions-to
-      [{:state :final-state
-        :if (fn [& args] true)}]}
-     :final-state}}))
-
-#_(deftest test-would-transition-once?
-  (is :next-state
-      (would-transition-once?
-       simple-workflow)))
-
-  ;; TODO: assert that would-transition? does not fire triggers/observers
-  ;; TODO: assert that transition-once! does fire triggers/observers
-  ;; TODO: assert that transition! does fire triggers/observers
+;; (test-would-transition?)
