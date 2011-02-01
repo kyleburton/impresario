7a5cf26aca2e507ec87e436e0e2e793372383c9d
Kyle Burton <kburton@relaynetwork.com>
2011-02-01 17:01:07 -0500

    implemented triggers: on-transition, on-entry, on-exit

--- src/impresario/core.clj
+++ src/impresario/core.clj
@@ -9,7 +9,6 @@
   (.split (.substring (str kwd) 1)
           "/"))
 
-
 (defn fn-lookup-via-symbol [sym-name]
   (let [[ns-name-part sym-name-part] (split-keyword-parts sym-name)]
     (ns-resolve (symbol ns-name-part)
@@ -44,15 +43,99 @@
                                    transitions)]
     (printf "transitions:%s\n" (vec transitions))
     (printf "viable-next-states:%s\n" (vec viable-next-states))
-    (if (= 1 (count viable-next-states))
+    (cond
+      (= 1 (count viable-next-states))
       (:state (first viable-next-states))
-      nil)))
+
+      (zero? (count viable-next-states))
+      nil
+
+      :else
+      (throw
+       (RuntimeException.
+        (format "Error: multiple target states! workflow:%s curr:%s viable-targets:%s"
+                (:name workflow)
+                current-state
+                (vec viable-next-states)))))))
 
 (defn transition? [workflow current-state state-store]
   (loop [prev-state current-state
          next-state (transition-once? workflow current-state state-store)]
     (printf "transition? prev-state:%s next-state:%s\n" prev-state next-state)
     (if (nil? next-state)
+      ;; we're done here
       prev-state
+      (recur next-state
+             (transition-once? workflow next-state state-store)))))
+
+(defn seqize-triggers [triggers]
+  (cond
+      (nil? triggers)
+      nil
+
+      (or (vector? triggers) (seq? triggers))
+      triggers
+
+      :else
+      [triggers]))
+
+(defn on-exit-triggers [workflow state]
+  (seqize-triggers (:on-exit (get (:states workflow) state))))
+
+(defn on-entry-triggers [workflow state]
+  (seqize-triggers (:on-entry (get (:states workflow) state))))
+
+(defn on-transition-triggers [workflow current-state next-state]
+  (let [state-info (filter (fn [state-info]
+                             (= next-state (:state state-info)))
+                           (:transitions (get (:states workflow)
+                                              current-state)))]
+    (printf "on-transition-triggers: %s to %s : %s" current-state next-state (vec state-info))
+    (cond
+      (empty? state-info)
+      (throw
+       (RuntimeException.
+        (format "Error: unable to find transition from %s to %s?? While looking up transition triggers."
+                current-state
+                next-state)))
+
+      (> 1 (count state-info))
+      (RuntimeException.
+        (format "Error: found more than 1 transition from %s to %s?? While looking up transition triggers."
+                current-state
+                next-state))
+
+      :else
+      (seqize-triggers (:on-transition (first state-info))))))
+
+(defn execute-trigger [trigger workflow current-state next-state state-info]
+  (let [f (resolve-predicate trigger)]
+    (f workflow current-state next-state state-info)))
+
+(defn execute-triggers [workflow current-state next-state state-store]
+  (doseq [trigger (on-exit-triggers workflow current-state)]
+    (execute-trigger trigger workflow current-state next-state state-store))
+  (doseq [trigger (on-transition-triggers workflow current-state next-state)]
+    (execute-trigger trigger workflow current-state next-state state-store))
+  (doseq [trigger (on-entry-triggers workflow next-state)]
+    (execute-trigger trigger workflow current-state next-state state-store)))
+
+(defn transition-once! [workflow current-state state-store]
+  (let [next-state (transition-once? workflow current-state state-store)]
+    (if (nil? next-state)
+      ;; no transition
+      current-state
+      (do
+        (execute-triggers workflow current-state next-state state-store)
+        next-state))))
+
+(defn transition! [workflow current-state state-store]
+  (loop [prev-state current-state
+         next-state (transition-once! workflow current-state state-store)]
+    (printf "transition! prev-state:%s next-state:%s\n" prev-state next-state)
+    (if (nil? next-state)
+      ;; we're done here, didn't transition
+      prev-state
+      ;; keep trying to transition
       (recur next-state
              (transition-once? workflow next-state state-store)))))
--- test/impresario/test/core.clj
+++ test/impresario/test/core.clj
@@ -2,57 +2,109 @@
   (:use [impresario.core] :reload)
   (:use [clojure.test]))
 
-;; (def door-spec
-;;   {:name "Front Door"
-;;    ;; NB: what about constructor generate state?  Should we support a function that returns the initial state?
-;;    ;; :internal-store :some.namespace/make-initial-state
-;;    :internal-store {:locked? false
-;;                     :key-id  "761098709817145"}
-;;    ;; :triggers [{:type :timer {:timer-data-goes-here "1234 4312 abcd"}}
-;;    ;;            {:type :on-wakeup :some-function]
-;;    :states  {:open   {:start true
-;;                       :transitions [{:state :closed :if :close-door?}]}
-;;              :closed {:transitions [{:state :open   :if :can-open-door?}
-;;                                        {:state :locked :if :lock-door?}]}
-;;              :locked {:transitions [{:state :closed :if :unlock-door?}]}}})
-
-;; TODO: assert that would-transition? does not fire triggers/observers
-;; TODO: assert that transition-once! does fire triggers/observers
-;; TODO: assert that transition! does fire triggers/observers
-
 (defn transition-every-time [workflow current-state state-store]
   (printf "transition-every-time, returning true...\n")
   true)
 
 (def simple-workflow
-    {:states
-     {:first-state
-      {:start true
-       :transitions
-       [{:state :next-state
-         :if :impresario.test.core/transition-every-time}]}
-      :next-state
-      {:transitions
-       [{:state :final-state
-         :if :impresario.test.core/transition-every-time}]}
-      :final-state
-      {:transitions []}}})
-
+  {:name :simple-workflow
+   :states
+   {:first-state
+    {:start true
+     :transitions
+     [{:state :next-state
+       :if :impresario.test.core/transition-every-time}]}
+    :next-state
+    {:transitions
+     [{:state :final-state
+       :if :impresario.test.core/transition-every-time}]}
+    :final-state
+    {:transitions []}}})
 
 (deftest test-would-transition-once?
-    (is (= :next-state
-           (transition-once?
-            simple-workflow
-            :first-state
-            {}))))
+  (is (= :next-state
+         (transition-once?
+          simple-workflow
+          :first-state
+          {}))))
 
 ;; (test-would-transition-once?)
 
 (deftest test-would-transition?
-    (is (= :final-state
-           (transition?
-            simple-workflow
-            :first-state
-            {}))))
+  (is (= :final-state
+         (transition?
+          simple-workflow
+          :first-state
+          {}))))
 
 ;; (test-would-transition?)
+
+;; can we step it 1 state transition at a time
+;; start -> next-state -> final-state
+
+(deftest test-error-on-multiple-vaible-states
+  (let [workflow
+        {:name :simple-workflow
+         :states
+         {:first-state   {:start true
+                          :transitions
+                          [{:state :second-state
+                            :if :impresario.test.core/transition-every-time}
+                           {:state :final-state
+                            :if :impresario.test.core/transition-every-time}]}
+          :second-state  {:transitions []}
+          :final-state   {:transitions []}}}]
+    (is (thrown?
+         RuntimeException
+         (transition-once? workflow :first-state {})))))
+
+;; (test-error-on-multiple-vaible-states)
+
+;; support triggers
+(def *last-trigger* (atom nil))
+
+(defn simple-workflow-trigger [workflow curr-state next-state store]
+  (printf "simple-workflow-trigger: curr-state:%s store=%s\n" curr-state store)
+  (reset! *last-trigger* next-state))
+
+(def simple-workflow-with-triggers
+  {:name :simple-workflow-with-triggers
+   :states
+   {:first-state
+    {:start true
+     :on-entry :impresario.test.core/simple-workflow-trigger
+     :transitions
+     [{:state :next-state
+       :if :impresario.test.core/transition-every-time
+       :on-transition :impresario.test.core/simple-workflow-trigger}]}
+    :next-state
+    {:on-entry :impresario.test.core/simple-workflow-trigger
+     :transitions
+     [{:state :final-state
+       :if :impresario.test.core/transition-every-time
+       :on-transition :impresario.test.core/simple-workflow-trigger}]}
+    :final-state
+    {:transitions []}}})
+
+;; *last-trigger*
+;; (on-exit-triggers simple-workflow-with-triggers :first-state)
+;; (on-entry-triggers simple-workflow-with-triggers :first-state)
+;; (on-transition-triggers simple-workflow-with-triggers :first-state :next-state)
+
+(deftest test-triggers-transition-once
+  (let [res-state (transition-once!
+                   simple-workflow-with-triggers
+                   :first-state
+                   {})]
+    (is (= :next-state res-state))
+    (is (= :next-state @*last-trigger*))
+    (transition-once! simple-workflow-with-triggers
+                      :next-state
+                      {})
+    (is (= :final-state @*last-trigger*))))
+
+;; (test-triggers-transition-once)
+
+
+
+;; (run-all-tests)
