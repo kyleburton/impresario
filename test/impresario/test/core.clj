66a2f2c2cb7ac641e6eebf7d5fbb204f5a8d6274
Kyle Burton <kburton@relaynetwork.com>
2011-02-02 18:05:49 -0500

    introduced group before clojars push

--- test/impresario/test/core.clj
+++ test/impresario/test/core.clj
@@ -117,10 +117,13 @@
     (pp/pprint new-context)
     (is (= :next-state res-state))
     (is (= :next-state @*last-trigger*))
-    (transition-once! simple-workflow-with-triggers
-                      :next-state
-                      {})
-    (is (= :final-state @*last-trigger*))))
+    (let [[res-state new-context] (transition-once! simple-workflow-with-triggers
+                                                    res-state
+                                                    new-context)]
+      (is (= :final-state @*last-trigger*))
+      (printf "new-context:\n")
+      (pp/pprint new-context))))
+
 
 ;; (test-triggers-transition-once)
 
@@ -130,33 +133,33 @@
 ;; (spit "examples/simple-workflow.dot" (workflow-to-dot simple-workflow-with-triggers :start))
 
 (comment
- (spit "examples/simple-workflow2.dot"
-       (workflow-to-dot
-        {:name :simple-workflow-with-triggers
-         :states
-         {:first-state
-          {:start true
-           :on-entry :impresario.test.core/simple-workflow-trigger
-           :transitions
-           [{:state :next-state
-             :if :impresario.test.core/transition-every-time
-             :on-transition :impresario.test.core/simple-workflow-trigger}
-            {:state :intermediate-state
-             :if :impresario.test.core/transition-every-time
-             :on-transition :impresario.test.core/simple-workflow-trigger}]}
-          :intermediate-state
-          {:on-entry :impresario.test.core/simple-workflow-trigger
-           :transitions
-           [{:state :final-state
-             :if :impresario.test.core/transition-every-time
-             :on-transition :impresario.test.core/simple-workflow-trigger}]}
-          :next-state
-          {:on-entry :impresario.test.core/simple-workflow-trigger
-           :transitions
-           [{:state :final-state
-             :if :impresario.test.core/transition-every-time
-             :on-transition :impresario.test.core/simple-workflow-trigger}]}
-          :final-state
-          {:transitions []}}}
-        :first-state))
- )
+  (spit "examples/simple-workflow2.dot"
+        (workflow-to-dot
+         {:name :simple-workflow-with-triggers
+          :states
+          {:first-state
+           {:start true
+            :on-entry :impresario.test.core/simple-workflow-trigger
+            :transitions
+            [{:state :next-state
+              :if :impresario.test.core/transition-every-time
+              :on-transition :impresario.test.core/simple-workflow-trigger}
+             {:state :intermediate-state
+              :if :impresario.test.core/transition-every-time
+              :on-transition :impresario.test.core/simple-workflow-trigger}]}
+           :intermediate-state
+           {:on-entry :impresario.test.core/simple-workflow-trigger
+            :transitions
+            [{:state :final-state
+              :if :impresario.test.core/transition-every-time
+              :on-transition :impresario.test.core/simple-workflow-trigger}]}
+           :next-state
+           {:on-entry :impresario.test.core/simple-workflow-trigger
+            :transitions
+            [{:state :final-state
+              :if :impresario.test.core/transition-every-time
+              :on-transition :impresario.test.core/simple-workflow-trigger}]}
+           :final-state
+           {:transitions []}}}
+         :first-state))
+  )
