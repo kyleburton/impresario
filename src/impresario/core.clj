21c32f04c3744beaa9dd33387a5a215b51769a7c
Paul and Kyle <kburton@relaynetwork.com>
2011-09-30 18:04:30 -0400

    put uuid in before init

--- src/impresario/core.clj
+++ src/impresario/core.clj
@@ -322,7 +322,7 @@
                       (reduce
                        (fn [m state]
                          (assoc m state 0))
-                       {}
+                       {:uuid (str (java.util.UUID/randomUUID))}
                        (keys (:states workflow)))}
                      context)
         context      (assoc-in context [:state-tracking start-state] 1)]
@@ -332,10 +332,7 @@
       (if trigger
         (recur triggers
                (execute-trigger trigger workflow nil start-state context))
-        (assoc
-            context
-          :uuid (str (java.util.UUID/randomUUID)))))))
-
+        context))))
 
 (defn path-tracing-trigger [workflow curr-state next-state context]
   (assoc
